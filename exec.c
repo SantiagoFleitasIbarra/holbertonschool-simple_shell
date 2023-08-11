66504907448853a12bab3991e4d33217e3a6c955
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-08-11 08:32:42 -0700

    added function aux in exit

--- exec.c
+++ exec.c
@@ -24,7 +24,7 @@ void execute_command(char **args)
 void execute_input(char **args)
 {
 	if (strcmp(args[0], "exit") == 0)
-		exit(0);
+		handle_exit(args);
 	else if (strcmp(args[0], "env") == 0)
 	{
 		char **env = environ;
