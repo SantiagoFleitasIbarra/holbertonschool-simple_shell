7ae10c18b486f7fa5d9272e110ca7b83cd1cdedc
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-08-11 16:19:44 -0700

    moved from the test repo

--- shell.h
+++ shell.h
@@ -1,5 +1,5 @@
-#ifndef SHELL_H
-#define SHELL_H
+# ifndef _SHELL_H_
+# define _SHELL_H_
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,10 +7,20 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
-#define MAX_ARGS 10
-#define MAX_COMMAND_LENGTH 40
-void execute_command(char *command);
+#include <fcntl.h>
+#include <sys/stat.h>
+#define MAX_INPUT_SIZE 1024
+extern char **environ;
 void display_prompt(void);
+void execute_command(char **args);
+int is_absolute_path(char *command);
+void handle_noninteractive_mode(void);
+void tokenize_input(char *input, char **args);
+void execute_input(char **args);
+void search_and_execute(char **args);
 int main(void);
-void parse_command(char *input, char **args, int *argc);
-#endif
+void handle_cd(char **args);
+void handle_help(void);
+void handle_setenv(char **args);
+void handle_unsetenv(char **args);
+# endif
