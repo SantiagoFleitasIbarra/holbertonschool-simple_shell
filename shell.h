48e5b8efb072fa076658b61edc4d1922893d0dcb
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-08-11 14:18:50 -0700

    added reference function

--- shell.h
+++ shell.h
@@ -6,8 +6,11 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <errno.h>
+#define MAX_ARGS 10
+#define MAX_COMMAND_LENGTH 40
 void execute_command(char *command);
 void display_prompt(void);
 int main(void);
-#define MAX_COMMAND_LENGTH 40
+void parse_command(char *input, char **args, int *argc);
 #endif
