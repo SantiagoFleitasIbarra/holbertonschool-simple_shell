483cac55a06d9a160d941f90fab2b1e1a65835ed
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-08-12 15:13:06 -0700

    execution process

--- exec.c
+++ exec.c
@@ -1,142 +1,37 @@
 # include "shell.h"
 /**
- * execute_command - manages the execution of external commands in the shell
- * @args: string arrangement
+ * execute_command - execution of external commands in the shell
+ * @args: argument string
 */
 void execute_command(char **args)
 {
-	pid_t pid = fork();
+	char *command = args[0];
+	int status;
+	pid_t pid;
 
+	pid = fork();
 	if (pid == 0)
 	{
-		if (execvp(args[0], args) == -1)
+		char *full_path = get_absolute_path(command);
+
+		if (full_path == NULL)
+			full_path = search_path(command);
+		if (full_path != NULL)
 		{
-			perror("Error");
+			execv(full_path, args);
+			perror(command);
+			free(full_path);
 			exit(EXIT_FAILURE);
 		}
-	}
-	else if (pid > 0)
-	{
-		wait(NULL);
-	}
-	else
-		perror("Fork failed");
-}
-/**
- * execute_input - executes a command provided as an argument
- * @args: string arrangement
-*/
-void execute_input(char **args)
-{
-	int i;
-
-	if (strcmp(args[0], "exit") == 0)
-		exitt(args);
-	else if (strcmp(args[0], "env") == 0)
-	{
-		char **env = environ;
-
-		while (*env)
+		else
 		{
-			printf("%s\n", *env);
-			env++;
+			fprintf(stderr, "%s: Command not found\n", command);
+			exit(EXIT_FAILURE);
 		}
-	} else if (strcmp(args[0], "cd") == 0)
-	{
-		handle_cd(args);
-	} else if (strcmp(args[0], "help") == 0)
-	{
-		handle_help();
-	} else if (strcmp(args[0], "setenv") == 0)
-	{
-		handle_setenv(args);
-	} else if (strcmp(args[0], "unsetenv") == 0)
-	{
-		handle_unsetenv(args);
-	}
+	} else if (pid < 0)
+		perror("fork");
 	else
 	{
-		if (is_absolute_path(args[0]))
-		{
-			if (access(args[0], X_OK) == 0)
-				execute_command(args);
-			else
-				fprintf(stderr, "hsh: %s No such file or directory\n", args[0]);
-		} else
-			search_and_execute(args);
+		waitpid(pid, &status, 0);
 	}
-	for (i = 0; args[i] != NULL; i++)
-		free(args[i]);
-}
-/**
- * search_and_execute - search for a command in the PATH locations
- * @args: an arrangement of strings
-*/
-void search_and_execute(char **args)
-{
-	char *path = getenv("PATH");
-
-	if (args[0] == NULL)
-		return;
-
-	if (path)
-	{
-		char path_copy[MAX_INPUT_SIZE];
-		char *dir = strtok(path_copy, ":");
-		int found = 0;
-
-		strcpy(path_copy, path);
-		while (dir)
-		{
-			char command_path[MAX_INPUT_SIZE];
-
-			snprintf(command_path, sizeof(command_path), "%s/%s", dir, args[0]);
-			if (access(command_path, X_OK) == 0)
-			{
-				execute_command(args);
-				found = 1;
-				break;
-			}
-			dir = strtok(NULL, ":");
-		}
-		if (!found)
-		{
-			if (strchr(args[0], '/') != NULL)
-				fprintf(stderr, "%s: No such file or directory\n", args[0]);
-			else if (dir == NULL)
-				fprintf(stderr, "hsh: %s: command not found\n", args[0]);
-			else
-				fprintf(stderr, "%s: Is a directory\n", args[0]);
-		}
-	}
-	else
-	{
-		fprintf(stderr, "%s: variable PATH not configured\n", args[0]);
-	}
-}
-/**
- * _atoi - converts a string into an integer
- *@s: pointer to a string
- *Return: the integer
- */
-int _atoi(char *s)
-{
-	int i, integer, sign = 1;
-
-	i = 0;
-	integer = 0;
-	while (!((s[i] >= '0') && (s[i] <= '9')) && (s[i] != '\0'))
-	{
-		if (s[i] == '-')
-		{
-			sign = sign * (-1);
-		}
-		i++;
-	}
-	while ((s[i] >= '0') && (s[i] <= '9'))
-	{
-		integer = (integer * 10) + (sign * (s[i] - '0'));
-		i++;
-	}
-	return (integer);
 }
