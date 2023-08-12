6c7c9cd39dc29bd6c90103f85be448a897bd85e2
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-08-12 15:14:43 -0700

    added reference functions and structure

--- shell.h
+++ shell.h
@@ -1,26 +1,39 @@
-# ifndef _SHELL_H_
-# define _SHELL_H_
+#ifndef SHELL_H
+#define SHELL_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <libgen.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <sys/stat.h>
 #define MAX_INPUT_SIZE 1024
+#define MAX_TOKENS 100
+
+/**
+ * struct InternalCommand - structure for internal commands
+ * @name: stores the name of the internal command
+ * @function: is a pointer to a function
+*/
+typedef struct InternalCommand
+{
+	char *name;
+	void (*function)(char **args);
+} InternalCommand;
+
+struct InternalCommand *get_internal_commands();
+
 extern char **environ;
-void display_prompt(void);
+
+void cd_command(char **args);
+void exit_command(char **args);
+void env_command(char **args);
+void execute_external_command(char **args);
+char *search_path(char *command);
+char *get_absolute_path(char *command);
 void execute_command(char **args);
-int is_absolute_path(char *command);
-void handle_noninteractive_mode(void);
-void tokenize_input(char *input, char **args);
-void execute_input(char **args);
-void search_and_execute(char **args);
 int main(void);
-void handle_cd(char **args);
-void handle_help(void);
-void handle_setenv(char **args);
-void handle_unsetenv(char **args);
-# endif
+void display_prompt(void);
+int tokenize_input(char *input, char *tokens[], int max_tokens);
+
+#endif
