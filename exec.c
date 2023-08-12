3bd74e2d8741b3861623faeec3a53fed6d173e32
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-08-11 19:22:15 -0700

    added a function aux

--- exec.c
+++ exec.c
@@ -31,7 +31,7 @@ void execute_input(char **args)
 	int i;
 
 	if (strcmp(args[0], "exit") == 0)
-		exit(0);
+		exitt(args);
 	else if (strcmp(args[0], "env") == 0)
 	{
 		char **env = environ;
@@ -113,4 +113,30 @@ void search_and_execute(char **args)
 	{
 		fprintf(stderr, "%s: variable PATH not configured\n", args[0]);
 	}
+}
+/**
+ * _atoi - converts a string into an integer
+ *@s: pointer to a string
+ *Return: the integer
+ */
+int _atoi(char *s)
+{
+	int i, integer, sign = 1;
+
+	i = 0;
+	integer = 0;
+	while (!((s[i] >= '0') && (s[i] <= '9')) && (s[i] != '\0'))
+	{
+		if (s[i] == '-')
+		{
+			sign = sign * (-1);
+		}
+		i++;
+	}
+	while ((s[i] >= '0') && (s[i] <= '9'))
+	{
+		integer = (integer * 10) + (sign * (s[i] - '0'));
+		i++;
+	}
+	return (integer);
 }
